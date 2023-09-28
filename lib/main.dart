743462176747c0be54fe311c1dc8c6df1e451859
jogna <jordan.gnamien@smile.ci>
2023-09-28 15:10:51 +0000

    -- Jogna git commit --

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,68 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(''),
+                    style: TextButton.styleFrom(backgroundColor: Colors.red),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(''),
+                    style: TextButton.styleFrom(backgroundColor: Colors.orange),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(''),
+                    style: TextButton.styleFrom(backgroundColor: Colors.yellow),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(''),
+                    style: TextButton.styleFrom(backgroundColor: Colors.green),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(''),
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.teal.shade800),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(''),
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.blue.shade700),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(''),
+                    style: TextButton.styleFrom(backgroundColor: Colors.purple),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
